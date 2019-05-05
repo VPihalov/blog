6cd24d6f6a0ce2cc90fb00aed910bab9b3d35568
V.Pigalov <pratimpuls@gmail.com>
2019-05-05 21:42:44 +0300

    template is transmitted

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require('./models/post');
 const app = express();
 const config = require('./config');
 const path = require('path');
@@ -11,29 +10,18 @@ global.dirProject = __dirname;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, config.get('dirweb'))));
+app.use(
+    '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 app.get("/", (req, res) => {
-    Post.find({}).then(posts => {
-        res.render("index", { posts: posts });
-    })
+    res.render('index')
 }
   // res.render("index", { arr: arr });
 );
 
-app.get("/create", (req, res) => {
-  res.render("create");
-  console.log('to the create Post')
-});
-
-app.post("/create", (req, res) => {
-  const {title, body} = req.body;
-  Post.create({
-      title: title,
-      body: body
-  }).then(post => console.log(`post.id: ${post.id}`));
-  res.redirect("/");
-  console.log('...Post.create')
-});
 
 database().then(info => {
   console.log(`\x1b[34;1;40mConnected to Datebase ${info.host}:${info.port}/${info.name}!\x1b[34;1;40m`);
